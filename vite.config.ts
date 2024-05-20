0936cf79547c061fdbfb99d0153965f54e3aa929
Awesome Dev <149862258+awesomedev82@users.noreply.github.com>
2024-05-20 03:32:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://abi-rai.github.io/QuizSiteReact",
+  base: "https://react-quiz-app-sooty.vercel.app",
   plugins: [react()],
   server: {
     watch: {
